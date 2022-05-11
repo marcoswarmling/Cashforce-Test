0b919fcc1d99ee16e0414d5645acb40210a7ad14
marcoswarmling <marcoswarmling15@gmail.com>
2022-05-11 16:23:41 -0300

    ADD: adicionando a base em vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Invoices></Invoices>
+</template>
+
+<script>
+import Invoices from './components/Invoices'
+
+export default {
+  name: 'App',
+  components: {
+    Invoices,
+  },
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
