a13a7885e6964500d63cafdf4801aeb1532cd7a2
marcoswarmling <marcoswarmling15@gmail.com>
2022-05-12 19:15:01 -0300

    ADD: adicionando o css

--- frontend/src/components/Invoices.vue
+++ frontend/src/components/Invoices.vue
@@ -1,31 +1,50 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
+    rel="stylesheet"
+  />
   <div class="page">
-    <div class="sider-financiador">
+    <div class="frame-subtitle">
+      <div>
+        <img src="../assets/Vector.png" alt="" />
+        <h3 class="subtitle">Notas fiscais</h3>
+      </div>
+      <div class="visualize-propostas">
+        Visualise as notas fiscais que voce tem.
+      </div>
+    </div>
       <div class="sider-fornecedor">
         <div class="menu-hero">
           <div class="sider-logo">
-            <h1 class="logo">Logo</h1>
+            <img src="../assets/logo.png" alt="logo da empresa cachforce" />
           </div>
           <div />
           <div class="menu-item">
-            <h2 class="title">Notas fiscais</h2>
+            <img
+              src="../assets/Rectangle_69.png"
+              class="rectangle-img"
+              alt="um retangulo que diz que a aba notas fiscais esta selecionada"
+            />
+            <img
+              src="../assets/emoji.png"
+              class="emoji-image"
+              alt="emoji de cumprimento formal"
+            />
+            <h2 class="selected-title">Notas fiscais</h2>
           </div>
-        </div>
       </div>
-    </div>
-    <div class="page-header">
-      <h3>Notas fiscais</h3>
-      <div>Visualise as notas fiscais que voce tem.</div>
     </div>
     <div class="frame-704">
       <div class="table-header">
         <div class="rectangle-224">
-          <div class="nota-fiscal"></div>
-          <div class="sacado"></div>
-          <div class="cedente"></div>
-          <div class="emissao"></div>
-          <div class="valor"></div>
-          <div class="status"></div>
+          <div class="nota-fiscal">NOTA FISCAL</div>
+          <div class="sacado">SACADO</div>
+          <div class="cedente">CEDENTE</div>
+          <div class="emissao">EMISSÂO</div>
+          <div class="valor">VALOR</div>
+          <div class="status">STATUS</div>
         </div>
       </div>
       <div v-for="items in values" :key="items.value" class="table-box">
@@ -42,10 +61,15 @@
           {{ items.emissionDate }}
         </div>
         <div class="table-box-invoice-value">
-          {{ items.value }}
+         R$ {{ items.value }}
         </div>
         <div class="table-box-invoice-status">
-          {{ items.orderStatusBuyer }}
+          {{ ordersSwitch(items.orderStatusBuyer) }}
+        </div>
+        <div class="btn-box">
+          <a class="btn">
+            <div class="btn-text">Dados do cedente</div>
+          </a>
         </div>
       </div>
     </div>
@@ -55,7 +79,6 @@
 <script>
 import "../styles/Invoice.css";
 import axios from "axios";
-//import axios from 'axios'
 export default {
   name: "InvoicesTable",
   data() {
@@ -74,8 +97,30 @@ export default {
         console.log(response.data);
       });
     },
+    ordersSwitch(orders) {
+      switch (orders) {
+        case "0":
+          return "Pendente de confirmação";
+        case "1":
+          return "Pedido confirmado";
+        case "2":
+          return "Não reconhece o pedido";
+        case "3":
+          return "Mercadoria não recebida";
+        case "4":
+          return "Recebida com avaria";
+        case "5":
+          return "Devolvida";
+        case "6":
+          return "Recebida com devolução parcial";
+        case "7":
+          return "Recebida e confirmada";
+        case "8":
+          return "Pagamento Autorizado";
+        default:
+          return "";
+      }
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to divmit CSS to this component only -->
